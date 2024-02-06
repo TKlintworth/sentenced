d9c19ff469ecb1b3d7d5f901621537629e81fffb
Tristan Klintworth <frostflag@gmail.com>
2024-02-05 21:28:57 -0500

    skeleton frontend and backend

---
+++ frontend/sentenced-frontend/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { onMount } from 'svelte';
+    import io from 'socket.io-client';
+  
+    let socket;
+  
+    onMount(() => {
+      socket = io('http://localhost:3000');
+  
+      socket.on('connect', () => {
+        console.log('Connected to server');
+      });
+    });
+  </script>

8b64b00c73b96f8b5ee97169b3b83dcd1a041a62
Tristan Klintworth <frostflag@gmail.com>
2024-02-17 00:21:02 -0500

    play page layout

--- frontend/src/routes/play/+page.svelte
+++ frontend/src/routes/play/+page.svelte
@@ -1,27 +1,61 @@
 <script>    
     import WordContainer from '../../components/WordContainer.svelte';
+    import RoundCounter from '../../components/RoundCounter.svelte';
+    import RoundTimer from '../../components/RoundTimer.svelte';
 
     import { draggable } from "$lib/dnd";
 
 </script>
-
 <div class="playScreenContainer">
+    <!-- <div class="flex justify-between items-center w-full">
+        <div class="basis-1/2 flex justify-center">
+            <RoundCounter></RoundCounter>
+        </div>
+        <div class="basis-1/2 flex justify-end pr-4">
+            <RoundTimer></RoundTimer>
+        </div>
+    </div> -->
     <div class="topWordContainers">
         <WordContainer type="words"></WordContainer>
         <WordContainer type="words"></WordContainer>
     </div>
-    <WordContainer type="sentence"></WordContainer>
-    <WordContainer type="words"></WordContainer>
+    <div class="sentenceContainer">
+        <WordContainer type="sentence"></WordContainer>
+    </div>
+    <div class="bottomWordContainers">
+        <WordContainer type="words"></WordContainer>
+        <WordContainer type="words"></WordContainer>
+    </div>
 </div>
 
 
 <style>
-    .topWordContainers {
+    .playScreenContainer {
         display: flex;
-        margin-top: 1%;
+        flex-direction: column;
+        justify-content: space-evenly;
     }
 
-    
+    .sentenceContainer {
+        width: 85vw;
+    }
+
+    .topWordContainers {
+        display: flex;
+        width: 100vw;
+        align-items: stretch;
+    }
+
+    .bottomWordContainers {
+        display: flex;
+        margin-bottom: 3%;
+        width: 100vw;
+        align-items: stretch;
+    }
+
+    :global(.topWordContainers) > * {
+        width: 50vw;
+    }
 </style>
 
 

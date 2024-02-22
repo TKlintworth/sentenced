365c1a88c55a3c1c50086c79301d9ca02364cfeb
Tristan Klintworth <frostflag@gmail.com>
2024-02-22 00:16:41 -0500

    start styling server browser page

--- frontend/src/components/Lobby.svelte
+++ frontend/src/components/Lobby.svelte
@@ -5,17 +5,25 @@
     export let lobbyId;
     export let lobbyData;
 
-
+    function attemptToJoinLobby() {
+        console.log('Attempting to join lobby: ', lobbyId);
+        socketStore.subscribe((socket) => {
+            if (socket){
+                // Try to join the lobby
+                // socket.emit('join-lobby', lobbyId);
+            }
+        });
+    }
 
 </script>
 
-<div class="card card-compact bg-base-100 shadow-x2">
+<div class="card card-compact bg-de-york-100 shadow-x2 m-4 transition-colors duration-300 ease-in-out hover:bg-de-york-300">
     <div class="card-body">
-        <h2 class="card-title">{lobbyData.host}'s Lobby</h2>
+        <h2 class="card-title">{lobbyData.hostPlayerName}'s Lobby</h2>
         <p>Players: {lobbyData.userCount}/{lobbyData.maxUsers}</p>
         <p>Status: {lobbyData.status}</p>
         <div class="card-actions justify-end">
-            <button class="btn btn-primary">Join Lobby</button>
+            <button class="btn bg-de-york-600" on:click={attemptToJoinLobby}>Join Lobby</button>
         </div>
     </div>
 </div>

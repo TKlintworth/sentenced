cf785b075aea31e492e471b26cdf113145075e7e
Tristan Klintworth <frostflag@gmail.com>
2024-02-23 22:37:46 -0500

    lobby list and lobby card progress

--- frontend/src/components/Lobby.svelte
+++ frontend/src/components/Lobby.svelte
@@ -5,8 +5,8 @@
     export let lobbyId;
     export let lobbyData;
 
-    function attemptToJoinLobby() {
-        console.log('Attempting to join lobby: ', lobbyId);
+    function requestToJoinLobby() {
+        console.log('Requesting to join lobby: ', lobbyId);
         socketStore.subscribe((socket) => {
             if (socket){
                 // Try to join the lobby
@@ -19,15 +19,30 @@
 
 <div class="card card-compact bg-de-york-100 shadow-x2 m-4 transition-colors duration-300 ease-in-out hover:bg-de-york-300">
     <div class="card-body">
-        <h2 class="card-title">{lobbyData.hostPlayerName}'s Lobby</h2>
+        <div class="lobby-header">
+            {#if lobbyData.password}
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mb-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                </svg>
+            {/if}
+            <h2 class="card-title">{lobbyData.serverName}</h2>
+        </div>
+        <p>Host: {lobbyData.hostPlayerName}</p>
         <p>Players: {lobbyData.userCount}/{lobbyData.maxUsers}</p>
         <p>Status: {lobbyData.status}</p>
         <div class="card-actions justify-end">
-            <button class="btn bg-de-york-600" on:click={attemptToJoinLobby}>Join Lobby</button>
+            <button class="btn bg-de-york-600" on:click={requestToJoinLobby}>Join Lobby</button>
         </div>
     </div>
 </div>
 
 <style>
+    button {
+        color: #ECF0F1;
+    }
 
+    .lobby-header {
+        display: flex;
+        align-items: center;
+    }
 </style>
--- frontend/src/components/LobbyList.svelte
+++ frontend/src/components/LobbyList.svelte
@@ -1,34 +1,64 @@
 <script>
     import { onMount } from 'svelte';
+    import { goto } from '$app/navigation';
     import { socketStore } from '../lib/socketStore.js';
     import Lobby from './Lobby.svelte';
     
     let lobbies = [];
 
-    socketStore.subscribe((socket) => {
-        if (socket){
-            setInterval(() => {
+    onMount(() => {
+        socketStore.subscribe((socket) => {
+            if (socket){
                 socket.emit('list-lobbies');
-                console.log('Sent list-lobbies request')
-            }, 10000);
-
-            socket.on('list-lobbies', (newLobbies) => {
-                console.log('Lobbies: ', newLobbies);
-                lobbies = newLobbies;
-            });
-        }
+                console.log('Sent list-lobbies request');
+                socket.on('list-lobbies', (newLobbies) => {
+                    console.log('Lobbies: ', newLobbies);
+                    lobbies = newLobbies;
+                });
+                socket.on('lobby-created', () => {
+                    socket.emit('list-lobbies');
+                });
+            }
+        });
     });
-    
+
+    function createLobbyButtonClicked() {
+        goto('/servers/create');
+    }
+
+    function refreshButtonClicked() {
+        console.log('Refresh button clicked');
+        socketStore.subscribe((socket) => {
+            if (socket){
+                socket.emit('list-lobbies');
+                console.log('Sent list-lobbies request');
+            }
+        });
+    }
+
+    function backButtonClicked() {
+        goto('/');
+    }
 
 </script>
 
-<div class="lobby-list">
-    {#each Object.entries(lobbies) as [lobbyId, lobby]}
-        <Lobby lobbyId={lobbyId} lobbyData={lobby} />
-    {/each}
+<div>
+    <div class="lobby-list">
+        {#each Object.entries(lobbies) as [lobbyId, lobby]}
+            <Lobby lobbyId={lobbyId} lobbyData={lobby} />
+        {/each}
+    </div>
+    <div class="lobby-list-buttons">
+        <button class="btn bg-de-york-600 text-cod-gray-100" on:click={createLobbyButtonClicked}>Create Lobby</button>
+        <button class="btn bg-de-york-600 text-cod-gray-100" on:click={refreshButtonClicked}>Refresh</button>
+        <button class="btn bg-de-york-600 text-cod-gray-100" on:click={backButtonClicked}>Back</button>
+    </div>
 </div>
 
 <style>
     .lobby-list {
+    }
+
+    .lobby-list-buttons {
     }
 </style>

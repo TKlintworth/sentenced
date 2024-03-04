a3488276fc1e42e6fd277e2329b667b43fdbc9e1
Tristan Klintworth <frostflag@gmail.com>
2024-03-03 20:34:04 -0500

    incremental detect when user leaves lobby

--- frontend/src/routes/servers/[serverId]/+page.svelte
+++ frontend/src/routes/servers/[serverId]/+page.svelte
@@ -1,13 +1,25 @@
 <script>
     // Get the values from +page.js
     import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
     import { socketStore } from '$lib/socketStore.js';
     import UserList from "../../../components/UserList.svelte";
+    import { page } from '$app/stores';
 
     export let data;
     let users = [];
 
     $: console.log(data);
+
+    onMount = () => {
+        socketStore.subscribe((socket) => {
+            if (socket) {
+                console.log('Subscribed to socket');
+                $socketStore = socket;
+            }
+        });
+    }
+
 
     // Get the users in this server
     $socketStore.emit('lobby-players-request', data.props.serverId, retrievedUsersCallback);
@@ -17,11 +29,17 @@
         users = data;
     }
 
+    onbeforeunload = () => {
+        console.log('Leaving lobby');
+        $socketStore.emit('leave-lobby', data.props.serverId);
+    }
+
     // Get the server ID from the URL
     //$: serverId = $page.params.serverId;
 </script>
 <div>
     <UserList serverId={data.props.serverId} users={users}></UserList>
     <h1>Server ID: {data.props.serverId}</h1>
+    <p>Current path: {$page.url}</p>
 
 </div>

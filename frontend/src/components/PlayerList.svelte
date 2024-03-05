8b20483229a9427777d8080a40fe25c452a1f332
Tristan Klintworth <frostflag@gmail.com>
2024-03-04 22:04:24 -0500

    minor changes unashamed streak day drove 8 hours

---
+++ frontend/src/components/PlayerList.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { onMount } from 'svelte';
+    import { goto } from '$app/navigation';
+    import { socketStore } from '../lib/socketStore.js';
+
+    
+</script>
--- frontend/src/components/RoundTimer.svelte
+++ frontend/src/components/RoundTimer.svelte
@@ -1,25 +1,29 @@
 <script>
-    let timeLeft = 60; // Starting time left in seconds
-  
-    // Countdown function
-    const countdown = () => {
-      if(timeLeft > 0) {
-        setTimeout(() => {
-          timeLeft -= 1;
-          countdown();
-        }, 1000);
-      }
-    };
-  
-    countdown(); // Start the countdown
+  import { onMount } from 'svelte';
+  import { goto } from '$app/navigation';
+  import { socketStore } from '../lib/socketStore.js';
+
+  export let roundTime = 60; // Default round time in seconds
+
+  // Countdown function
+  const countdown = () => {
+    if(roundTime > 0) {
+      setTimeout(() => {
+        roundTime -= 1;
+        countdown();
+      }, 1000);
+    }
+  };
+
+  countdown(); // Start the countdown
 </script>
 
 <div class="flex justify-center items-center h-screen">
     <div class="text-center">
         <div class="text-2xl font-bold text-blue-500 mb-2">Time Left in Round</div>
         <div class="text-6xl font-semibold text-blue-700">
-            {timeLeft}
+            {roundTime}
         </div>
-        <progress class="progress progress-primary w-56" value={timeLeft} max="60"></progress>
+        <progress class="progress progress-primary w-56" value={roundTime} max="60"></progress>
     </div>
 </div>

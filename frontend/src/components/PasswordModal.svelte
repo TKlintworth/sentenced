df4c66bc3f15c3d6d08c37555fe4bb70e83da0ee
Tristan Klintworth <frostflag@gmail.com>
2024-03-06 23:45:37 -0500

    use daisyui modal component for password modal

--- frontend/src/components/PasswordModal.svelte
+++ frontend/src/components/PasswordModal.svelte
@@ -18,24 +18,14 @@
 </script>
   
 {#if showModal}
-<div class="fixed z-10 inset-0 overflow-y-auto">
-    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-        <div class="fixed inset-0 transition-opacity" aria-hidden="true">
-            <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
-        </div>
-
-        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-
-        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-            <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <h3 class="text-lg leading-6 font-medium text-gray-900">Enter Lobby Password</h3>
-                <input type="password" bind:value={password} class="mt-2 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-            </div>
-            <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm" on:click={handleSubmit}>Submit</button>
-                <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" on:click={handleCancel}>Cancel</button>
-            </div>
-        </div>
+  <div class="modal modal-middle modal-open">
+    <div class="modal-box">
+      <h3 class="font-bold text-lg">Enter Lobby Password</h3>
+      <input type="password" bind:value={password} class="input input-bordered w-full mt-4" />
+      <div class="modal-action">
+        <button class="btn btn-primary" on:click={handleSubmit}>Submit</button>
+        <button class="btn" on:click={handleCancel}>Cancel</button>
+      </div>
     </div>
-</div>
+  </div>
 {/if}

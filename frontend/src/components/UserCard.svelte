922af00666d3c11b92f300dd7a5cb44e0884f1ab
Tristan Klintworth <frostflag@gmail.com>
2024-03-14 23:55:01 -0400

    user card ready

--- frontend/src/components/UserCard.svelte
+++ frontend/src/components/UserCard.svelte
@@ -3,17 +3,11 @@
   export let ready;
 </script>
 
-<div class="card bg-base-100 shadow-xl class:bg-de-york-400={ready}">
+<div class="card shadow-xl" class:bg-base-100={!ready} class:bg-de-york-500={ready}>
   <div class="card-body">
     <h2 class="card-title">{user.name}</h2>
     <p>Joined at: {user.createdAt}</p>
     <p>Lobby: {user.lobbyName || 'Not in a lobby'}</p>
-    <div class="">
-      {#if ready}
-        <button on:click={} class="btn bg-de-york-600 text-cod-gray-100">Ready</button>
-      {:else}
-        <button class="btn bg-de-york-600 text-cod-gray-100">Not Ready</button>
-      {/if}
-    </div>
+    <p>{ready ? 'Ready' : 'Not Ready'}</p>
   </div>
 </div>

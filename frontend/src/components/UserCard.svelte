5485c019d7b3211b524d02a873eb514e36ba39ff
Tristan Klintworth <frostflag@gmail.com>
2024-03-14 23:42:18 -0400

    start adding ready status to user card

--- frontend/src/components/UserCard.svelte
+++ frontend/src/components/UserCard.svelte
@@ -1,11 +1,19 @@
 <script>
   export let user;
+  export let ready;
 </script>
 
-<div class="card bg-base-100 shadow-xl">
+<div class="card bg-base-100 shadow-xl class:bg-de-york-400={ready}">
   <div class="card-body">
     <h2 class="card-title">{user.name}</h2>
     <p>Joined at: {user.createdAt}</p>
     <p>Lobby: {user.lobbyName || 'Not in a lobby'}</p>
+    <div class="">
+      {#if ready}
+        <button on:click={} class="btn bg-de-york-600 text-cod-gray-100">Ready</button>
+      {:else}
+        <button class="btn bg-de-york-600 text-cod-gray-100">Not Ready</button>
+      {/if}
+    </div>
   </div>
 </div>

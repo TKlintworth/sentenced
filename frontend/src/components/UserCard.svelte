91de23a7f595678ab8b725065ba65fc2816e700a
Tristan Klintworth <frostflag@gmail.com>
2024-04-09 00:04:25 -0400

    remove field from usercard

--- frontend/src/components/UserCard.svelte
+++ frontend/src/components/UserCard.svelte
@@ -6,7 +6,7 @@
 <div class="card shadow-xl" class:bg-base-100={!ready} class:bg-de-york-500={ready}>
   <div class="card-body">
     <h2 class="card-title">{user.name}</h2>
-    <p>Joined at: {user.createdAt}</p>
+    <!-- <p>Joined at: {user.createdAt}</p> -->
     <p>Lobby: {user.lobbyName || 'Not in a lobby'}</p>
     <p>{ready ? 'Ready' : 'Not Ready'}</p>
   </div>

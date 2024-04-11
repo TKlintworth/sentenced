f0f070cb655e327d974bbd30566f0242bb08e777
Tristan Klintworth <frostflag@gmail.com>
2024-04-10 21:54:26 -0400

    game state store

---
+++ backend/game/gameState.js
@@ -0,0 +1,20 @@
+// Store game state 
+
+import { writable } from 'svelte/store';
+
+export const gameState = writable({
+    game: {
+        id: 0,
+        name: 'Game',
+        description: 'Game description',
+        status: 'waiting',
+        players: [],
+        maxPlayers: 2,
+        minPlayers: 2,
+        currentPlayer: 0,
+        winner: null
+    },
+    players: [],
+    currentPlayer: 0,
+    winner: null
+});
